9cb49b98f30f1afd812b6de61121322bf645ed54
mlieou <newlmd@gmail.com>
2016-07-30 21:14:03 -0400

    solution to ex 7.3.2

--- ch7/7_23_24_27.hpp
+++ ch7/7_23_24_27.hpp
@@ -2,6 +2,7 @@
 #define SCREENS_OBJ
 
 #include <string>
+#include <iostream>
 
 class Screen {
     public:
@@ -9,26 +10,53 @@ class Screen {
         Screen() = default;
         Screen(pos h, pos w, char c) : height(h), width(w), contents(h * w, c) {};
         Screen(pos h, pos w) : height(h), width(w), contents(h * w, ' ') {};
-        char get() const {
+
+        inline char get() const {
             return contents[cursor];
         }
         inline char get(pos h, pos w) const;
         inline Screen &move(pos r, pos c);
+        inline Screen &set(pos, pos, char);
+        inline Screen &set(char);
+
+        Screen &display(std::ostream &os) {
+            do_display(os);
+            return *this;
+        }
+
+        const Screen &display(std::ostream &os) const {
+            do_display(os);
+            return *this;
+        }
+        
     private:
         pos cursor = 0;
         pos height = 0, width = 0;
         std::string contents;
+        void do_display(std::ostream &os) const {
+            os << contents;
+        }
 };
 
 Screen &Screen::move(pos r, pos c) {
     pos row = r * width;
     cursor = row + c;
-    rethrn *this;
+    return *this;
 }
 
 char Screen::get(pos r, pos c) const {
     pos row = r * width;
     return contents[row + c];
+}
+
+Screen &Screen::set(pos row, pos col, char ch) {
+    contents[row * width + col] = ch;
+    return *this;
+}
+
+Screen &Screen::set(char c) {
+    contents[cursor] = c;
+    return *this;
 }
 
 #endif
---
+++ ch7/7_27.cpp
@@ -0,0 +1,10 @@
+#include "7_23_24_27.hpp"
+
+int main() {
+    Screen myScreen(5, 5, 'X');
+    myScreen.move(4, 0).set('#').display(std::cout);
+    std::cout << std::endl;
+    myScreen.display(std::cout);
+    std::cout << std::endl;
+    return 0;
+}
