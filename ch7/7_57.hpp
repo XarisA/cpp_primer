19493a31c6f23ac2a92c93e59ce93ff126794caf
Mlieou <newlmd@gmail.com>
2016-08-01 11:52:40 -0400

    solution to ex 7.5.6 and 7.6

---
+++ ch7/7_57.hpp
@@ -0,0 +1,25 @@
+#include <string>
+
+class Account {
+    public:
+        void calculate() { amount += amount * interestRate; }
+        static double rate() { return interestRate; }
+        static void rate(double);
+    private:
+        std::string owner;
+        double amount;
+        static double interestRate;
+        static double initRate();
+        static constexpr double currentRate = 0.01;
+};
+
+void Account::rate(double newRate) {
+    interestRate = newRate;
+}
+
+double Account::initRate() {
+    return currentRate;
+}
+
+double Account::interestRate = initRate();
+
