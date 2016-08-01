651dc69af97ea2c5aa4f76c69cc27ecefcaa7e00
Mlieou <newlmd@gmail.com>
2016-08-01 15:11:11 -0400

    solution to 8.2.1

---
+++ ch8/8_4.cpp
@@ -0,0 +1,25 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::ifstream;
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char **argv) {
+    ifstream in(argv[1]);
+    vector<string> vec;
+    if (in) {
+        string buff;
+        while (getline(in, buff)) {
+            vec.push_back(buff);
+        }
+    }
+    for (auto i = vec.begin(); i!= vec.end(); ++i) {
+        cout << *i << endl;
+    }
+    return 0;
+}
