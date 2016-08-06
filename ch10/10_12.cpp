5240ebbfa3e9bc4d3fc6a3758f07d834dad55b08
Mlieou <newlmd@gmail.com>
2016-08-06 12:54:19 -0400

    solution to 10.3.1

---
+++ ch10/10_12.cpp
@@ -0,0 +1,31 @@
+#include "../ch07/7_41.hpp"
+#include <vector>
+#include <algorithm>
+
+using std::sort;
+using std::vector;
+
+bool compareIsbn(const Sales_data &d1, const Sales_data &d2) {
+    return d1.isbn() < d2.isbn();
+}
+
+int main() {
+    vector<Sales_data> sales;
+    sales.emplace_back("123");
+    sales.emplace_back("32512");
+    sales.emplace_back("13453");
+    sales.emplace_back("213432");
+    sales.emplace_back("2314");
+    sales.emplace_back("375");
+    sales.emplace_back("0971023");
+    sales.emplace_back("198247234");
+    sales.emplace_back("123");
+    sales.emplace_back("124");
+    sort(sales.begin(), sales.end(), compareIsbn);
+    for (const auto &s : sales) {
+        cout << s.isbn() << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
--- ch10/10_9_11.cpp
+++ ch10/10_9_11.cpp
@@ -7,10 +7,11 @@ using std::string;
 using std::cout;
 using std::endl;
 using std::sort;
+using std::stable_sort;
 using std::unique;
 
 void print(const vector<string> &s) {
-    for (auto &word : s) {
+    for (const auto &word : s) {
         cout << word << ' ';
     }
     cout << endl;
@@ -25,9 +26,16 @@ void elimDups(vector<string> &words) {
     print(words);
 }
 
+bool isShorter(const string &s1, const string &s2) {
+    return s1.size() < s2.size();
+}
+
 int main() {
     vector<string> vec{"the", "quick", "red", "fox", "jumps", "over", "the", "slow", "red", "turtle"};
     elimDups(vec);
+
+    stable_sort(vec.begin(), vec.end(), isShorter);
+    print(vec);
 
     return 0;
 }
