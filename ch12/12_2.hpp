b3bb02c71407ae52f0f77343229593e6620831f3
Mlieou <newlmd@gmail.com>
2016-08-14 11:50:18 -0400

    add solution to 12.1.1

---
+++ ch12/12_2.hpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+#include <memory>
+
+class StrBlob {
+public:
+    typedef std::vector<std::string>::size_type size_type;
+    StrBlob() : data(std::make_shared<std::vector<std::string>>()) {}
+    StrBlob(std::initializer_list<std::string> il) : data(std::make_shared<std::vector<std::string>>(il)) {}
+    size_type size() const { return data->size(); }
+    bool empty() const { return data-> empty(); }
+
+    // add and remove elements
+    void push_back(const std::string &t) { data->push_back(t); }
+    void pop_back() { 
+        check(0, "pop_back on empty StrBlob");
+        data->pop_back();
+    }
+
+    // element access
+    std::string& front() {
+        check(0, "front on empty StrBlob");
+        return data->front();
+    }
+    const std::string& front() const {
+        check(0, "front on empty StrBlob");
+        return data->front();
+    }
+    std::string& back() {
+        check(0, "back on empty StrBlob");
+        return data->back();
+    }
+    const std::string& back() const {
+        check(0, "back on empty StrBlob");
+        return data->back();
+    }
+private:
+    std::shared_ptr<std::vector<std::string>> data;
+    
+    // throws msg if data[i] isn't vaild
+    void check(size_type i, const std::string &msg) const {
+        if (i >= data->size()) throw std::out_of_range(msg);
+    }
+};
+
