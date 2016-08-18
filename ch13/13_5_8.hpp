fe1bafc699256a328d64ef98c70d63cb1e75d2e7
Mlieou <newlmd@gmail.com>
2016-08-18 15:36:11 -0400

    Add solution to Exercise 13.8

--- ch13/13_5_8.hpp
+++ ch13/13_5_8.hpp
@@ -8,6 +8,16 @@ public:
     HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0) {}
     HasPtr(const HasPtr &orig) : ps(new std::string(*orig.ps)), i(orig.i) { }
                                                     // equivalent to *(orig.ps), see page 166
+
+    HasPtr& operator=(const HasPtr &rhs) {
+        if (this != &rhs) {
+            std::string* temp = new std::string(*rhs.ps);
+            delete ps;
+            ps = temp;
+            i = rhs.i;
+        }
+        return *this;
+    }                                             
 private:
     std::string *ps;
     int i;
