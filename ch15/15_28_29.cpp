68c4fc4b97dda3747f0fb527744ba0baa0d8e582
Mlieou <newlmd@gmail.com>
2016-09-09 13:28:45 -0400

    add solution to ex 15.29

--- ch15/15_28_29.cpp
+++ ch15/15_28_29.cpp
@@ -3,6 +3,23 @@
 #include <vector>
 
 int main() {
+    // ex 15.28
+    std::vector<Quote> sliced_down;
+    sliced_down.push_back(Bulk_quote("0-111-999-01", 5.2, 10, .51));
+    sliced_down.push_back(Bulk_quote("0-111-999-02", 15.3, 70, .25));
+    sliced_down.push_back(Bulk_quote("0-111-999-03", 2.1, 10, .15));
+    sliced_down.push_back(Bulk_quote("0-111-999-04", 77, 8, 0.5));
+    sliced_down.push_back(Bulk_quote("0-111-999-05", 125, 12, 0.5));
+
+    double total;
+    for (const auto &p : sliced_down) {
+        total += p.net_price(10);
+    }
+    std::cout << total << std::endl;
+
+
+    // ex 15.29
+    total = 0;
     std::vector<std::shared_ptr<Quote>> bucket;
     bucket.push_back(std::make_shared<Bulk_quote>("0-111-999-01", 5.2, 10, .51));
     bucket.push_back(std::make_shared<Bulk_quote>("0-111-999-02", 15.3, 70, .25));
@@ -10,7 +27,6 @@ int main() {
     bucket.push_back(std::make_shared<Bulk_quote>("0-111-999-04", 77, 8, 0.5));
     bucket.push_back(std::make_shared<Bulk_quote>("0-111-999-05", 125, 12, 0.5));
 
-    double total;
     for (const auto p : bucket) {
         total += p->net_price(10);
     }
