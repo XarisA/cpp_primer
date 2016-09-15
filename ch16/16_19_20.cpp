56c16f2f3db07504848a56a1b06f8a0b77ba19a9
Mlieou <newlmd@gmail.com>
2016-09-15 14:43:27 -0400

    add solution to ex 16.20

--- ch16/16_19_20.cpp
+++ ch16/16_19_20.cpp
@@ -8,9 +8,17 @@ void print(const T& container) {
     }
 }
 
+template <typename T>
+void print_iter(const T& container) {
+    for(auto p = container.cbegin(); p != container.cend(); ++p) {
+        std::cout << *p << ' ';
+    }
+}
+
 int main() {
     std::vector<int> vec{1, 2, 3, 4, 5, 6, 7};
     print(vec);
-    
+    std::cout << std::endl;
+    print_iter(vec);
     return 0;
 }
