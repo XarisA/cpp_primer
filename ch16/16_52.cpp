7e57d928ced5b8a13ca2316b0be4fe5cd24670e7
Mlieou <newlmd@gmail.com>
2016-09-19 13:52:18 -0400

    add solution to ex 16.52

---
+++ ch16/16_52.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+template <typename ... Args>
+void g(Args ... args) {
+    std::cout << "Args: " << sizeof...(Args) << std::endl;
+    std::cout << "args: " << sizeof...(args) << std::endl;
+}
+
+int main() {
+    g(1, 2);
+    g(1, 1.1);
+    char c('c');
+    g("this", 8.8, c);
+    return 0;
+}
