932ff66b40a9fdfd444addc9b4ee9aed9992718f
Mlieou <newlmd@gmail.com>
2016-09-22 14:06:44 -0400

    add solution to ex 17.20

---
+++ ch17/17_20.cpp
@@ -0,0 +1,29 @@
+#include <regex>
+#include <iostream>
+
+bool vaild(const std::smatch &m) {
+    if (m[1].matched)
+        return m[3].matched && (!m[4].matched || m[4].str() == " ");
+    else
+        return !m[3].matched && m[4].str() == m[6].str();
+}
+
+int main() {
+    std::string phone_num =
+        "(\\()?(\\d{3})(\\))?([-. ])?(\\d{3})([-. ]?)(\\d{4})";
+    std::regex r(phone_num);
+    std::smatch m;
+    std::string s;
+    while(getline(std::cin, s)) {
+        for (std::sregex_iterator it(s.begin(), s.end(), r), end_it;
+                it != end_it; ++it) {
+            if(vaild(*it))
+                std::cout << "Valid: ";
+            else
+                std::cout << "Not valid: ";
+            std::cout << it->str() << std::endl;
+        }
+    }
+
+    return 0;
+}
